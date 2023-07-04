a39445dff27eac6eeb7b1b78dcaf84e4bd7f6b0b
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-07-04 14:34:05 +0300

    EDSL

--- Hello.hs
+++ Hello.hs
@@ -1,33 +1,34 @@
 import Text.XHtml (content)
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
+myhtml :: Html
 myhtml = 
-    makeHtml
+    html_ 
     "lhbg"
-    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+    (append_(h1_ "Hello, world!") (p_ "Let's learn about Haskell!"))
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+html_ :: Title -> Structure -> Html
+html_ title (Structure content) = 
+   Html (el "head" (el "title" title) <> el "body" content)
 
-html_ :: String -> String
-html_ = el "html"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" 
 
-body_ :: String -> String
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
+p_ :: String -> Structure
+p_ = Structure . el "p"
+  
 el :: String -> String -> String
-el tag content =
+el tag content =  
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
+
+newtype Html = Html String
+  
+newtype Structure = Structure String
+
+type Title = String 
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render (Html html) = html
