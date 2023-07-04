727ea087e8c77ca8c5d0ff83462b8b3c22bc282c
Yevhen Koshmaryk <yevhen.koshmaryk@gmail.com>
2023-07-04 13:28:26 +0300

    Flexible HTML content

--- Hello.hs
+++ Hello.hs
@@ -1,3 +1,33 @@
+import Text.XHtml (content)
 main = putStrLn myhtml
 
-myhtml = "<html><body>Hello, world!</body></html>"
+myhtml :: String
+myhtml = 
+    makeHtml
+    "lhbg"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">" 
